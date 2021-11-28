14524b8ff021dbcbba9127e7ea42ad6946574c8a
nikkilkumar26 <nikkilkumar26@gmail.com>
2021-11-28 21:22:44 +0530

    react-bootstrap added!

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='container'>
+        <div className='container '>
             I'm a header
         </div>
     )
