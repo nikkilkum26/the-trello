8bb069f55283978b9fd1ada37a69d7932754214a
nikkilkumar26 <nikkilkumar26@gmail.com>
2021-11-29 14:05:35 +0530

    progress1

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,18 +9,29 @@ const Home = () => {
   }, []);
   console.log(workSpaceData);
   return (
-    <div className="container mt-3">
+    <div className="container mt-3 d-flex justify-content-evenly">
       {workSpaceData.map((workspace, idx) => (
         <div
-          className="shadow-sm p-3 mb-5 bg-body rounded text-center"
+          className=" card shadow-sm p-3 mb-5 bg-body rounded text-center"
           key={idx}
+          id="mycard"
         >
-          <div className="title">{workspace.title}</div>
+           <ul class="list-group list-group-flush">
+          <li className="title list-group-item">{workspace.title}</li>
           {workspace.cardList.map((eachCard, cardId) => (
-            <div key={cardId} className="cardlist">
+            <div className="d-flex justify-content-between">
+            <li key={cardId} className="cardlist mt-2 mb-2 list-group-item">
               {eachCard}
+            </li>
+            <i class="fa-regular fa-trash-can align-self-center"></i>
             </div>
           ))}
+        </ul>
+        <div className="add-card mt-2 mb-2 d-flex justify-content-between">
+            <label for="exampleInputEmail1" className="form-label align-self-center" id="card-name">card name</label>
+            <input className="form-control" id="card-name-input" />
+          </div>
+          <button type="submit" class="btn btn-dark">+ card</button>
         </div>
       ))}
     </div>
